93c211c0f85d229166f7ae80bf95409ac8cd5b02
RaoulWolf <raoul.wolf@ngi.no>
2022-07-20 08:43:00 +0200

    Small fix to the cas_identity column of `jp_cscl_*`

--- R/data-jp_cscl_ecs.R
+++ R/data-jp_cscl_ecs.R
@@ -12,13 +12,26 @@
 #'   Priority Assessment Chemical Substances, Monitoring Chemical Substances
 #'   and Class I/II Specified Chemical Substances). If a substance corresponds
 #'   to the "Existing Chemical substances", the following items are displayed.
+#'
+#'   About cas_identity:
+#'
+#'   * 1: The substance which matches the substance indicated by MITI number or
+#'     cabinet order number when a certain condition is fulfilled.
+#'     (The condition is listed in the remarks column of the search result, so
+#'     be sure to check it)
+#'   * 2: The substance which is regarded as CSCL: Class I Specified Chemical
+#'     Substances, Class II Specified Chemical Substances, Monitoring Chemical
+#'     Substances, Priority Assessment Chemical Substances by the operation
+#'     notification "Implementation of the Act on the Evaluation of Chemical
+#'     Substances and Regulation of Their Manufacture, etc. 3-1".
 #' @format A data frame with 56,975 rows and 5 variables:
 #' \describe{
 #'   \item{no}{(Integer) List number for each substance}
 #'   \item{chrip_id}{(Character) The CHemical Risk Information Platform (CHRIP)
 #'     ID}
 #'   \item{cas_rn}{(Character) Chemical Abstracts Service (CAS) registry number}
-#'   \item{cas_identity}{(Character) Additional information about the substance}
+#'   \item{cas_identity}{(Integer) Additional information about the substance,
+#'     see Description for details}
 #'   \item{chemical_substance_name}{(Character) The provided name for the
 #'     substance}
 #' }
